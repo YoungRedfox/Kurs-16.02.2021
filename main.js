20f0c2bf588f325b5dcb337448572c985bd71a70
Patryk Polus <poluspatryk.deweloper@gmail.com>
2021-02-16 18:23:35 +0100

    Kalkurator praca w toku

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
-.
+// let inputs = Array.from(document.querySelectorAll('input[name="result"], input[type="text"]'))
+
+// console.log(inputs)
+
+// Zmiana string do int
+// let changeToNumber = parseInt(input.value)
+
+function getValueAndChangeInput(value) {
+
+    // console.log(value)
+
+    let input = document.querySelector('input[name="result"]')
+
+    input.value += value
+
+}
+
+function resultCalc() {
+
+    let input = document.querySelector('input[name="result"]')
+
+    input.value = eval(input.value)
+    
+ 
+}
+
+function changeSympol() {
+
+    let input = document.querySelector('input[name="result"]')
+
+    let arrayInput = input.value.split(" ")
+
+    let getLastNumber = arrayInput[arrayInput.length - 1]
+
+    console.log(getLastNumber)
+
+}
